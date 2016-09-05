23ca77c2fa2e529dd441280e70a4395345bb01c4
Daniel Castañeda <daniel@kwantec.com>
2016-09-04 21:45:43 -0500

    Minimal HTTP server with Hello World page

--- hello/index.js
+++ hello/index.js
@@ -1,5 +1,32 @@
 /**
  * Created by Daniel Castañeda on 04/09/2016.
  */
+'use strict'
 
-console.log('Hello World NodeJS');
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+
+    home(req,res);
+
+});
+server.listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
+
+function home(req, res) {
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<!DOCTYPE \"html\">");
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title>Hello World NodeJS</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<h2>Hello World NodeJS!</h2>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+
+}
+
