54085dc3903f29b11c63f906a0eccc0ac7154e8d
Daniel Castañeda <daniel@kwantec.com>
2016-09-06 01:01:02 -0500

    Map sample: Calling Jasmine from Gulp test task

--- map/gulpfile.js
+++ map/gulpfile.js
@@ -4,8 +4,15 @@
 'use strict';
 
 var gulp = require('gulp');
+const jasmine = require('gulp-jasmine');
 
 gulp.task('default', function() {
     // place code for your default task here
     console.log('Hello Gulp');
+});
+
+gulp.task('test', function() {
+    gulp.src('spec/map/MapSpec.js')
+    // gulp-jasmine works on filepaths so you can't have any plugins before it
+        .pipe(jasmine());
 });
