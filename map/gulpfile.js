88fa2910766e27ebfa0527f185b5eb5ae435167e
Daniel Castañeda <daniel@kwantec.com>
2016-09-06 00:55:44 -0500

    Map sample: Added Hello Gulp

---
+++ map/gulpfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Daniel Castañeda on 06/09/2016.
+ */
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    // place code for your default task here
+    console.log('Hello Gulp');
+});
