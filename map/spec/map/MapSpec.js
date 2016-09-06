7d13816c51429fe2589b47bf4d06344e7ae98bb4
Daniel Castañeda <daniel@kwantec.com>
2016-09-05 23:18:18 -0500

    Map sample: Added Jasmine test infrastructure and initial test spec

---
+++ map/spec/map/MapSpec.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Daniel Castañeda on 05/09/2016.
+ */
+'use strict';
+
+describe("Map", function() {
+    var Map = require('../../lib/map');
+    var myMap;
+
+    beforeAll(function() {
+       console.log('Running Map tests');
+    });
+    beforeEach(function() {
+        myMap = new Map();
+    });
+
+    it("should say hi", function() {
+        console.log('Running sayHi Test');
+        myMap.sayHi();
+
+    });
+
+});
